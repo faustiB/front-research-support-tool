812d70660d07295353d2646205045ef346deedaa
Faozi Bouybaouene Gadrouz <faozi.bouybaouene@col.vueling.com>
2023-03-14 22:54:01 +0100

    ui changes on detail page

--- lib/app/ui/components/detail_page.dart
+++ lib/app/ui/components/detail_page.dart
@@ -3,7 +3,6 @@ import 'package:flutter/material.dart';
 import 'package:research_support_tool/app/models/journal_model.dart';
 import 'package:url_launcher/url_launcher.dart';
 
-
 class DetailPage extends StatelessWidget {
   final JournalModel journal;
 
@@ -26,125 +25,52 @@ class DetailPage extends StatelessWidget {
               Container(
                 padding: const EdgeInsets.only(bottom: 16),
                 alignment: Alignment.centerLeft,
-                child: Text("Description",
-                    style: Theme.of(context).textTheme.headline4),
+                child: Text("Description", style: Theme.of(context).textTheme.headline4),
               ),
               Text(
                 journal.description,
                 style: Theme.of(context).textTheme.bodyText1,
               ),
+              RowOfDetail(
+                title: "Editorial: ",
+                content: journal.editorial,
+              ),
+              RowOfDetail(
+                title: "ISSN: ",
+                content: journal.issn,
+              ),
+              RowOfDetail(
+                title: "ISSN-10: ",
+                content: journal.issn10,
+              ),
+              RowOfDetail(
+                title: "ISSN-13: ",
+                content: journal.issn13,
+              ),
+              RowOfDetail(
+                title: "Quartile: ",
+                content: journal.quartile,
+              ),
+              RowOfDetail(
+                title: "Quartile Year: ",
+                content: journal.quartileYear.toString(),
+              ),
               Row(
                 children: [
                   Container(
-                    padding: const EdgeInsets.only(top: 16,bottom: 16),
+                    padding: const EdgeInsets.only(top: 16, bottom: 16),
                     alignment: Alignment.centerLeft,
-                    child: Text("Editorial: ",
-                        style: Theme.of(context).textTheme.headline4),
+                    child: Text("Quartile Website: ", style: Theme.of(context).textTheme.headline4),
                   ),
-                  Text(
-                    journal.editorial,
-                    style: Theme.of(context).textTheme.bodyText1,
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(top: 16,bottom: 16),
-                        alignment: Alignment.centerLeft,
-                        child: Text("ISSN: ",
-                            style: Theme.of(context).textTheme.headline4),
-                      ),
-                      Text(
-                        journal.issn,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(top: 16,bottom: 16),
-                        alignment: Alignment.centerLeft,
-                        child: Text("ISSN-10: ",
-                            style: Theme.of(context).textTheme.headline4),
-                      ),
-                      Text(
-                        journal.issn10,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(top: 16,bottom: 16),
-                        alignment: Alignment.centerLeft,
-                        child: Text("ISSN-13: ",
-                            style: Theme.of(context).textTheme.headline4),
-                      ),
-                      Text(
-                        journal.issn13,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                    ],
-                  ),
-                ],
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(top: 16,bottom: 16),
-                        alignment: Alignment.centerLeft,
-                        child: Text("Quartile: ",
-                            style: Theme.of(context).textTheme.headline4),
-                      ),
-                      Text(
-                        journal.quartile,
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(top: 16,bottom: 16),
-                        alignment: Alignment.centerLeft,
-                        child: Text("Quartile Year: ",
-                            style: Theme.of(context).textTheme.headline4),
-                      ),
-                      Text(
-                        journal.quartileYear.toString(),
-                        style: Theme.of(context).textTheme.bodyText1,
-                      ),
-                    ],
-                  ),
-                  Row(
-                    children: [
-                      Container(
-                        padding: const EdgeInsets.only(top: 16,bottom: 16),
-                        alignment: Alignment.centerLeft,
-                        child: Text("Quartile Website: ",
-                            style: Theme.of(context).textTheme.headline4),
-                      ),
-
-                      InkWell(
-                        child: Text(
-                          journal.quartileWebpage,
-                          style: Theme.of(context).textTheme.bodyText1,
-                          overflow: TextOverflow.ellipsis,
-                        ),
-                        onTap: () async {
-                          launchUrl(Uri.parse(journal.quartileWebpage));
-                        },
-                      ),
-                    ],
+                  InkWell(
+                    child: Text(
+                      journal.quartileWebpage,
+                      style: Theme.of(context).textTheme.bodyText1,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    onTap: () async {
+                      launchUrl(Uri.parse(journal.quartileWebpage));
+                    },
                   ),
                 ],
               ),
@@ -152,6 +78,34 @@ class DetailPage extends StatelessWidget {
           ),
         ),
       ),
+    );
+  }
+}
+
+class RowOfDetail extends StatelessWidget {
+  final String title;
+  final String content;
+
+  const RowOfDetail({
+    super.key,
+    required this.title,
+    required this.content,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          padding: const EdgeInsets.only(top: 16, bottom: 16),
+          alignment: Alignment.centerLeft,
+          child: Text(title, style: Theme.of(context).textTheme.headline4),
+        ),
+        Text(
+          content,
+          style: Theme.of(context).textTheme.bodyText1,
+        ),
+      ],
     );
   }
 }
